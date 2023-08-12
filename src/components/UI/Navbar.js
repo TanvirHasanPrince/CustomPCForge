244523b47088ec544f18c99a233f255d7e446184
unknown <t.hasan.prince@gmail.com>
2023-08-13 05:59:56 +0600

    Navbar

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -1,128 +1,126 @@
-import Head from 'next/head';
-import React from 'react';
+import Head from "next/head";
+import React from "react";
 
 const Navbar = () => {
-
-
- return (
-   <>
-     <div className="navbar bg-neutral text-neutral-content">
-       <div className="navbar-start">
-         <div className="dropdown">
-           <label tabIndex={0} className="btn btn-ghost lg:hidden">
-             <svg
-               xmlns="http://www.w3.org/2000/svg"
-               className="h-5 w-5"
-               fill="none"
-               viewBox="0 0 24 24"
-               stroke="currentColor"
-             >
-               <path
-                 strokeLinecap="round"
-                 strokeLinejoin="round"
-                 strokeWidth="2"
-                 d="M4 6h16M4 12h8m-8 6h16"
-               />
-             </svg>
-           </label>
-           <ul
-             tabIndex={0}
-             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-           >
-             <li>
-               <a>CPU / Processor</a>
-             </li>
-             <li>
-               <a>Motherboard</a>
-             </li>
-             <li>
-               <a>RAM</a>
-             </li>
-             <li>
-               <a>Power Supply Unit</a>
-             </li>
-             <li>
-               <a>Storage Device</a>
-             </li>
-             <li>
-               <a>Monitor</a>
-             </li>
-             <li tabIndex={0}>
-               <details>
-                 <summary>Others</summary>
-                 <ul className="p-2">
-                   <li>
-                     <a>Mouse</a>
-                   </li>
-                   <li>
-                     <a>Keyboard</a>
-                   </li>
-                   <li>
-                     <a>Spearks</a>
-                   </li>
-                   <li>
-                     <a>UPS</a>
-                   </li>
-                   <li>
-                     <a>Router</a>
-                   </li>
-                 </ul>
-               </details>
-             </li>
-           </ul>
-         </div>
-         <a className="btn btn-ghost normal-case text-xl">CustomPCForge</a>
-       </div>
-       <div className="navbar-center hidden lg:flex">
-         <ul className="menu menu-horizontal px-1">
-           <li>
-             <a>CPU / Processor</a>
-           </li>
-           <li>
-             <a>Motherboard</a>
-           </li>
-           <li>
-             <a>RAM</a>
-           </li>
-           <li>
-             <a>Power Supply Unit</a>
-           </li>
-           <li>
-             <a>Storage Device</a>
-           </li>
-           <li>
-             <a>Monitor</a>
-           </li>
-           <li tabIndex={0}>
-             <details>
-               <summary>Others</summary>
-               <ul className="p-2">
-                 <li>
-                   <a>Mouse</a>
-                 </li>
-                 <li>
-                   <a>Keyboard</a>
-                 </li>
-                 <li>
-                   <a>Spearks</a>
-                 </li>
-                 <li>
-                   <a>UPS</a>
-                 </li>
-                 <li>
-                   <a>Router</a>
-                 </li>
-               </ul>
-             </details>
-           </li>
-         </ul>
-       </div>
-       <div className="navbar-end">
-         <a className="btn btn-accent">PC Builder</a>
-       </div>
-     </div>
-   </>
- );
+  return (
+    <>
+      <div className="navbar bg-neutral text-neutral-content">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>CPU / Processor</a>
+              </li>
+              <li>
+                <a>Motherboard</a>
+              </li>
+              <li>
+                <a>RAM</a>
+              </li>
+              <li>
+                <a>Power Supply Unit</a>
+              </li>
+              <li>
+                <a>Storage Device</a>
+              </li>
+              <li>
+                <a>Monitor</a>
+              </li>
+              <li tabIndex={0}>
+                <details>
+                  <summary>Others</summary>
+                  <ul className="p-2">
+                    <li>
+                      <a>Mouse</a>
+                    </li>
+                    <li>
+                      <a>Keyboard</a>
+                    </li>
+                    <li>
+                      <a>Spearks</a>
+                    </li>
+                    <li>
+                      <a>UPS</a>
+                    </li>
+                    <li>
+                      <a>Router</a>
+                    </li>
+                  </ul>
+                </details>
+              </li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost normal-case text-xl">CustomPCForge</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>CPU / Processor</a>
+            </li>
+            <li>
+              <a>Motherboard</a>
+            </li>
+            <li>
+              <a>RAM</a>
+            </li>
+            <li>
+              <a>Power Supply Unit</a>
+            </li>
+            <li>
+              <a>Storage Device</a>
+            </li>
+            <li>
+              <a>Monitor</a>
+            </li>
+            <li tabIndex={0}>
+              <details>
+                <summary>Others</summary>
+                <ul className="p-2">
+                  <li>
+                    <a>Mouse</a>
+                  </li>
+                  <li>
+                    <a>Keyboard</a>
+                  </li>
+                  <li>
+                    <a>Spearks</a>
+                  </li>
+                  <li>
+                    <a>UPS</a>
+                  </li>
+                  <li>
+                    <a>Router</a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn btn-accent">PC Builder</a>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,8 +6,8 @@ export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
 
   const wrappedComponent = (
-    <div style={{ width: "1440px", margin: "0 auto" }}>
-      <Component {...pageProps} />
+    <div  style={{ width: "1440px", margin: "0 auto" }}>
+      <Component  {...pageProps} />
     </div>
   );
 
