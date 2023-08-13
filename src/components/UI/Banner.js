d036934fca2060049e347e00b142806f6aa1f1db
unknown <t.hasan.prince@gmail.com>
2023-08-13 06:19:30 +0600

    [Done]- Added Framer Motion

--- src/components/UI/Banner.js
+++ src/components/UI/Banner.js
@@ -1,43 +1,45 @@
-import React from 'react';
+import React from "react";
 import Lottie from "lottie-react";
-import bannerAnimation from '../../assets/banner/animation_ll8l2on9.json'
+import bannerAnimation from "../../assets/banner/animation_ll8l2on9.json";
 import Typewriter from "typewriter-effect";
 
-const Banner = () => {
- return (
-   <div className="grid lg:grid-cols-2 md:grid-cols-1 grid-cols-1 gap-10 py-20">
-     <div className="bg-base rounded-xl flex items-center justify-center">
-       <Lottie animationData={bannerAnimation} loop={true} />
-     </div>
-     <div className="font-courier ">
-       <h1 className="lg:text-7xl md:text-5xl text-2xl text-center ">
-         Empowering Your Digital Vision.
-         <span className="text-accent text-3xl">
 
-           <Typewriter
-             options={{
-               strings: ["One Component at a Time!"],
-               autoStart: true,
-               loop: true,
-             }}
-           />
-         </span>
-       </h1>
-       <br></br>
-       <p className=" text-sm lg:text-lg text-center">
-         Welcome to CustomPCForge, your ultimate destination for building
-         the PC of your dreams! Explore a wide range of high-quality PC parts
-         and components, carefully curated to empower your creativity and
-         performance needs. Our user-friendly PC Builder tool lets you craft a
-         custom rig with ease, while our expert guides and community insights
-         ensure you&apos;re making informed decisions every step of the way.
-         Unleash your imagination and assemble the perfect PC that suits your
-         style and ambitions. Start building today and elevate your computing
-         experience!
-       </p>
-     </div>
-   </div>
- );
+const Banner = () => {
+  return (
+ 
+          <div className="grid lg:grid-cols-2 md:grid-cols-1 grid-cols-1 gap-10 py-20">
+            <div className="bg-base rounded-xl flex items-center justify-center">
+              <Lottie animationData={bannerAnimation} loop={true} />
+            </div>
+            <div className="font-courier ">
+              <h1 className="lg:text-7xl md:text-5xl text-2xl text-center ">
+                Empowering Your Digital Vision.
+                <span className="text-accent text-3xl">
+                  <Typewriter
+                    options={{
+                      strings: ["One Component at a Time!"],
+                      autoStart: true,
+                      loop: true,
+                    }}
+                  />
+                </span>
+              </h1>
+              <br></br>
+              <p className=" text-sm lg:text-lg text-center">
+                Welcome to CustomPCForge, your ultimate destination for building
+                the PC of your dreams! Explore a wide range of high-quality PC
+                parts and components, carefully curated to empower your
+                creativity and performance needs. Our user-friendly PC Builder
+                tool lets you craft a custom rig with ease, while our expert
+                guides and community insights ensure you&apos;re making informed
+                decisions every step of the way. Unleash your imagination and
+                assemble the perfect PC that suits your style and ambitions.
+                Start building today and elevate your computing experience!
+              </p>
+            </div>
+          </div>
+   
+  );
 };
 
 export default Banner;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,23 @@
 import "@/styles/globals.css";
-import Head from "next/head";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function App({ Component, pageProps }) {
 
   const getLayout = Component.getLayout || ((page) => page);
 
   const wrappedComponent = (
-    <div  style={{ width: "1440px", margin: "0 auto" }}>
-      <Component  {...pageProps} />
-    </div>
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0, y: 15 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 15 }}
+        transition={{ delay: 0.25 }}
+      >
+        <div style={{ width: "1440px", margin: "0 auto" }}>
+          <Component {...pageProps} />
+        </div>
+      </motion.div>
+    </AnimatePresence>
   );
 
   return getLayout(wrappedComponent);
